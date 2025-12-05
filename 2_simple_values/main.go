50a3d405f0e177cbece0a387bc2feaf3b5d543cb
SUJOY GARAI <sujoycode999@gmail.com>
2025-12-05 13:09:16 +0530

    Refactor code structure for improved readability and maintainability

---
+++ 2_simple_values/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	var a int = 10
+	var b float64 = 20.5
+	var c string = "Go Programming"
+	var d bool = true	
+	println("Integer:", a)	
+	println("Float:", b)
+	println("String:", c)
+	println("Boolean:", d)
+}
