73c62cf64794fbbcae96fadaecd93d963804f4ed
SUJOY GARAI <sujoycode999@gmail.com>
2025-12-06 01:07:58 +0530

    Add float variable and print statement for improved output clarity

--- 2_simple_values/main.go
+++ 2_simple_values/main.go
@@ -5,8 +5,12 @@ func main() {
 	var b float64 = 20.5
 	var c string = "Go Programming"
 	var d bool = true	
+	var e float64 = 14.7
+
+	// Print the values
 	println("Integer:", a)	
 	println("Float:", b)
 	println("String:", c)
 	println("Boolean:", d)
+	println("Don't forget me float:", e)
 }
