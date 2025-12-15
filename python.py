c9fcb28392eaf2f15bb8c891543b7f113ce17ee8
SUJOY GARAI <sujoycode999@gmail.com>
2025-12-15 11:35:15 +0530

    add a new func in py

--- python.py
+++ python.py
@@ -3,3 +3,15 @@ def p():
         print(i)
     p()
 p()
+
+# This code will result in a RecursionError due to infinite recursion.
+
+function_call_stack_limit = 1000  # Example limit for function call stack depth
+def safe_p(depth=0):
+    if depth < function_call_stack_limit:
+        for i in range(100):
+            print(i)
+        safe_p(depth + 1)
+    else:
+        print("Reached maximum recursion depth.")
+safe_p()
